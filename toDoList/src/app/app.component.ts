0003aa48fb11a5cd5e9fcf6d59a359413a0c1e3e
Si Cheng <sicheng0228@gmail.com>
2020-04-07 20:08:36 -0400

    toDoList

---
+++ toDoList/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { ServeToDoListService } from "./serve-to-do-list.service";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'toDoList';
+  todoList:string[];
+  toAdd:string;
+
+  constructor(private serve:ServeToDoListService){}
+
+  ngOnInit() {
+    this.todoList = this.serve.getItems();
+  }
+
+  additem(){
+    if(this.toAdd!=null){
+      this.serve.setItems(this.toAdd);
+    }
+    this.toAdd='';
+  }
+  deleteItem(key){
+    this.serve.deleteItems(key);
+  }
+  searchItems(value){
+    this.todoList= this.todoList.filter(e=>e.toLowerCase().includes(value));
+    if(value==''){
+      this.todoList = this.serve.getItems();
+    }
+  }
+}
---
+++ toDoList/src/app/serve-to-do-list.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServeToDoListService {
+  private items:string[]=["phone","laptop","earPhone","macbook","Sofa","Lamp","cup"];
+
+  constructor() { }
+
+  getItems(){
+    return this.items;
+  }
+  setItems(item:string){
+    this.items.push(item);
+  }
+  deleteItems(key){
+    this.items.splice(key, 1);
+  }
+}
